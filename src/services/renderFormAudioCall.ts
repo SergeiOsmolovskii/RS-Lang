37e489edd433d625553759537b7354529d865858
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2022-02-20 23:02:40 +0300

    feat: add backend

--- src/services/renderFormAudioCall.ts
+++ src/services/renderFormAudioCall.ts
@@ -25,27 +25,27 @@ export const renderGame = `
   <button class="svg-volume"></button>
 </div> 
 <div class="answers">
-  <div class="level-1">
+  <div class="level">
     <button class="answer-item answer-item-1">
       <span class="answer-1">1</span>
     </button>
   </div>
-  <div class="level-1">
+  <div class="level">
     <button class="answer-item answer-item-2">
       <span class="answer-2">2</span>
     </button>
   </div>
-  <div class="level-1">
+  <div class="level">
     <button class="answer-item answer-item-3">
       <span class="answer-3">3</span>
     </button>
   </div>
-  <div class="level-1">
+  <div class="level">
     <button class="answer-item answer-item-4">
       <span class="answer-4">4</span>
     </button>
   </div>
-  <div class="level-1">
+  <div class="level last-level">
     <button class="answer-item answer-item-5">
       <span class="answer-5">5</span>
     </button>
